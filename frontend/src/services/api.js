104c291a803b9ce6ad76b2f19a4e6d6fea84b1c4
Pranay Kallepu <pranaykallepu05@gmail.com>
2025-11-08 16:24:38 +0530

    Base-api-changed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_BASE_URL = 'http://localhost:8000';
-const API_BASE_URL = "https://quiz-ai-generator.vercel.app";
+const API_BASE_URL = "https://quiz-ai-generator-backend.vercel.app/";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
